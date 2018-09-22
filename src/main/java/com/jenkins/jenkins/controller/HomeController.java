b09181ad68ae8d2c8c55ea2663a0c0002a730f43
shashi.ram <shashiram01@gmail.com>
2018-09-22 13:04:13 +0530

    first commit

---
+++ src/main/java/com/jenkins/jenkins/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.jenkins.jenkins.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RequestMapping("/api/")
+@RestController
+public class HomeController {
+    @RequestMapping("/hellow")
+    public String home () {
+        return "hi";
+    }
+}
